263c9659062e0e477b36d2765bb5a59fe8f4aff1
Katya Daron <katya_daron@hotmail.com>
2023-07-02 15:36:16 -0400

    Initial commit

---
+++ src/test/java/katya/movieApp/MovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package katya.movieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
