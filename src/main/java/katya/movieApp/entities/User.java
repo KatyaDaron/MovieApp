707b8bf0052dc967284f847027f3a64c6ad80bb4
Katya Daron <katya_daron@hotmail.com>
2023-07-02 16:41:14 -0400

    Created User entity

---
+++ src/main/java/katya/movieApp/entities/User.java
@@ -0,0 +1,26 @@
+package katya.movieApp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+}
