a39182f4fa1eb56bff0fa2ecaa5fcff354dbba7e
Katya Daron <katya_daron@hotmail.com>
2023-07-05 12:23:33 -0400

    Created HTML and JavaScript files for login and register functionality

---
+++ src/main/resources/static/javascript/register.js
@@ -0,0 +1,38 @@
+const registerForm = document.getElementById('register-form');
+const registerUsername = document.getElementById('register-username');
+const registerEmail = document.getElementById('register-email');
+const registerPassword = document.getElementById('register-password');
+
+const headers = {
+    'Content-Type' : 'application/json'
+}
+
+const baseUrl = 'http://localhost:8080/api/v1/users'
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let bodyObj = {
+        name: registerUsername.value,
+        email: registerEmail.value,
+        password: registerPassword.value
+    }
+
+    const response = await fetch(`${baseUrl}/register`, {
+        method: "POST",
+        body: JSON.stringify(bodyObj),
+        headers: headers
+    })
+        .catch(err => console.error(err.message));
+
+    const responseArr = await response.json()
+
+    if (response.status === 200) {
+        window.location.replace(responseArr[0])
+    } else {
+        const warningMessage = "User with this email already registered.";
+        alert(warningMessage);
+        }
+}
+
+registerForm.addEventListener("submit", handleSubmit)
