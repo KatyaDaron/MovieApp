797472a8ccbb63be6a5b409246a6be57d495a709
Katya Daron <katya_daron@hotmail.com>
2023-07-15 20:04:21 -0400

    Displayed average movie ratings on the main page

--- src/main/resources/static/javascript/home.js
+++ src/main/resources/static/javascript/home.js
@@ -6,7 +6,8 @@ const headers = {
     'Content-Type' : 'application/json'
 }
 
-const baseURL = "http://localhost:8080/api/movies/"
+const baseMoviesURL = "http://localhost:8080/api/movies/"
+const baseFeedbacksURL = "http://localhost:8080/api/feedbacks";
 
 const handleSubmit = async (e) => {
     e.preventDefault()
@@ -33,7 +34,7 @@ const handleSubmit = async (e) => {
 }
 
 async function addMovie(obj) {
-    const response = await fetch(`${baseURL}`, {
+    const response = await fetch(`${baseMoviesURL}`, {
         method: "POST",
         body: JSON.stringify(obj),
         headers: headers
@@ -45,7 +46,7 @@ async function addMovie(obj) {
 }
 
 async function getAllMovies() {
-    await fetch(`${baseURL}`, {
+    await fetch(`${baseMoviesURL}`, {
         method: "GET",
         headers: headers
     })
@@ -54,26 +55,83 @@ async function getAllMovies() {
         .catch(err => console.error(err))
 }
 
-function createMovieCard(movieArr) {
+async function createMovieCard(movieArr) {
     moviesContainer.innerHTML = '';
 
-    movieArr.forEach(movie => {
-        const movieCard = document.createElement('div');
-        movieCard.classList.add('movie-card');
+    const promises = movieArr.map(movie => {
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-card');
 
-        movieCard.innerHTML = `
-            <a href="movie.html?id=${movie.id}">
-                <img alt="movie cover" src=${movie.image} class="movie-cover"/>
-            </a>
-            <p class="movie-title">${movie.title}</p>
-        `;
+    movieCard.innerHTML = `
+        <a href="movie.html?id=${movie.id}">
+            <img alt="movie cover" src=${movie.image} class="movie-cover"/>
+        </a>
+    `;
 
-        moviesContainer.appendChild(movieCard);
+    const averageRatingPromise = getAverageRating(movie.id)
+        .then(averageRating => {
+            const movieDetails = document.createElement('div');
+            movieDetails.classList.add('movie-details');
+
+            const movieTitle = document.createElement('span');
+            movieTitle.classList.add('movie-title');
+            movieTitle.textContent = movie.title;
+            movieDetails.appendChild(movieTitle);
+
+            const ratingElement = document.createElement('span');
+            ratingElement.classList.add('movie-average-rating');
+
+            if (averageRating === 0) {
+                ratingElement.style.display = 'none';
+            } else {
+                const displayedRating = Number.isInteger(averageRating) ? averageRating.toString() : averageRating.toFixed(1);
+//                ratingElement.innerHTML = `<span>&#9733; ${displayedRating}</span>`;
+                ratingElement.innerHTML = `<span>${displayedRating}</span>`;
+            }
+
+            movieDetails.appendChild(ratingElement);
+            movieCard.appendChild(movieDetails);
+        });
+
+    moviesContainer.appendChild(movieCard);
+    return averageRatingPromise;
     });
+
+    await Promise.all(promises);
+}
+
+async function getAverageRating(movieId) {
+    try {
+        const response = await fetch(`${baseFeedbacksURL}/movie/${movieId}`, {
+            method: "GET",
+            headers: headers
+        });
+
+        if (response.ok) {
+            const feedbacks = await response.json();
+            const ratings = feedbacks.map((feedback) => feedback.ratingValue);
+            const averageRating = calculateAverageRating(ratings);
+            return averageRating;
+        } else {
+            console.error("Error fetching feedbacks:", response.status);
+        }
+    } catch (error) {
+        console.error("Error fetching feedbacks:", error);
+    }
+}
+
+function calculateAverageRating(ratings) {
+  if (ratings.length === 0) {
+    return 0;
+  }
+
+  const sum = ratings.reduce((total, rating) => total + rating, 0);
+  const average = sum / ratings.length;
+  return average;
 }
 
 async function movieSearch(query) {
-    await fetch(`${baseURL}search?query=${query}`, {
+    await fetch(`${baseMoviesURL}search?query=${query}`, {
         method: "GET",
         headers: headers
     })
